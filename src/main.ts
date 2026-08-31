3fe8e00301b1fb538ebfb469c4d8dd18d750f08a
= <=>
2026-09-01 01:02:08 +0900

    Initial Commit for Self Link Highlighter

--- src/main.ts
+++ src/main.ts
@@ -1,114 +1,31 @@
-import {
-	Editor,
-	MarkdownView,
-	MarkdownFileInfo,
-	Modal,
-	Notice,
-	Plugin,
-} from 'obsidian';
-import {
-	DEFAULT_SETTINGS,
-	MyPluginSettings,
-	SampleSettingTab,
-} from './settings';
+import { Plugin, MarkdownPostProcessorContext } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+export default class SelfLinkBoldPlugin extends Plugin {
+  onload(): void {
+    this.registerMarkdownPostProcessor(
+      (el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
+        // The note the user is actually looking at. Inside an embed this
+        // differs from ctx.sourcePath, which points at the embedded note.
 
-export default class MyPlugin extends Plugin {
-	settings!: MyPluginSettings;
+		// Potential Bug: This approach assumes a single pane, but can pick 
+		// the wrong note when two panes are open side by side.
+        const basePath =
+          this.app.workspace.getActiveFile()?.path ?? ctx.sourcePath;
 
-	async onload() {
-		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+        el.querySelectorAll<HTMLAnchorElement>('a.internal-link').forEach((a) => {
+          const href = (a.getAttribute('data-href') ?? '').replace(/[#|].*$/, '');
+          if (!href) return;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (
-				editor: Editor,
-				_ctx: MarkdownView | MarkdownFileInfo,
-			) => {
-				editor.replaceSelection('Sample editor command');
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			},
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(activeDocument, 'click', (_evt: MouseEvent) => {
-			new Notice('Click');
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000),
-		);
-	}
-
-	onunload() {}
-
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			(await this.loadData()) as Partial<MyPluginSettings>,
-		);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
+          // Resolved against sourcePath: a link written inside an embedded note
+          // must be resolved relative to that note, not the one being viewed.
+          const dest = this.app.metadataCache.getFirstLinkpathDest(
+            href,
+            ctx.sourcePath,
+          );
+          if (dest?.path === basePath) a.addClass('is-self-link');
+        });
+      },
+    );
+  }
 }
